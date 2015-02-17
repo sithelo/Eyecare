f5814233bd3eb08e3c7346af3b0f6e7c18fbc95e
Sithelo Ngwenya <sithelo@gmail.com>
2015-02-17 15:28:56 +0200

    added 2 projects

---
+++ eyecare.models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eyecare.models
+{
+    public class Class1
+    {
+    }
+}
